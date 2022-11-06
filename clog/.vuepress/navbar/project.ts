5c751a2ba0028f04834f8efc23f4ff7477869322
clean <1518136109@qq.com>
2022-11-06 21:57:33 +0800

    feat:add http module description

--- clog/.vuepress/navbar/project.ts
+++ clog/.vuepress/navbar/project.ts
@@ -1,9 +1,10 @@
-export default{ 
-  text: "项目文档", 
-  icon: "build", 
-  prefix: "/project/", 
+export default {
+  text: "项目文档",
+  icon: "build",
+  prefix: "/project/",
   children: [
     { text: "cyou-js", icon: "javascript", link: "cyou-js" },
     { text: "CYou", icon: "javascript", link: "CYou" },
-  ] 
+    { text: "clean-vite-cli", icon: "javascript", link: "clean-vite-cli" },
+  ]
 }
