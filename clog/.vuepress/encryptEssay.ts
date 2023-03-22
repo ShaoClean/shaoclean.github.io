20cdcd78cce9d32559948541a3c0f380ca1fa1cb
clean <1518136109@qq.com>
2023-03-22 22:39:19 +0800

    update encrypt config

--- clog/.vuepress/encryptEssay.ts
+++ clog/.vuepress/encryptEssay.ts
@@ -1,25 +1,42 @@
 import { getFiles } from "./getFiles";
-import fs from 'fs'
+import fs from "fs";
 
-export function encryptEssayConfig(){
-    let res = {}
-    let psw = ['sczscz']
+// 设置统一密码
+const psw = ["sczscz"];
+// 白名单，不需要.md
+const white_list = {
+    "2023": ["1"],
+};
 
-    function getYears(){
-        let res = fs.readdirSync(process.cwd() + '/clog/essay')
-        return res.filter(item=>item !== 'README.md')
-    }
+/**
+ * 文章加密配置
+ * @date 2023.3.22
+ * @returns 
+ */
+export function encryptEssayConfig() {
+	const res = {};
+	
+    // 获取essay目录下的第一层级文件夹名称（年份）
+	function getYears() {
+		let res = fs.readdirSync(process.cwd() + "/clog/essay");
+		return res.filter(item => item !== "README.md");
+	}
 
-    let years = getYears();
+	let years = getYears();
 
-    years.forEach(year=>{
-        let filesArr = getFiles(`/clog/essay/${year}`)
-        filesArr.forEach(eassy=>{
-            let key = `/essay/${year}/${eassy}.html`
-            res[key] = psw
-        })
-    })
-
-    return res;
+	years.forEach(year => {
+		let filesArr = getFiles(`/clog/essay/${year}`);
+		for (let eassy of filesArr) {
+            // 跳过加密
+			const isShow = white_list[year]?.includes(eassy);
+			if (isShow) {
+				continue;
+			}
+            // 加密
+			let key = `/essay/${year}/${eassy}.html`;
+			res[key] = psw;
+		}
+	});
+    
+	return res;
 }
-
